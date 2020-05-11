648ad41243fc0a8864d3d8542c2adc208e2316ae
UlanDossym <dossumov.ulan@gmail.com>
2020-05-11 23:43:27 +0200

    10.111 added product details

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/product-type';
 import {map} from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shop-params';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -38,10 +39,14 @@ export class ShopService {
         })
       );
   }
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
   }
   getTypes() {
     return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
+
 }
