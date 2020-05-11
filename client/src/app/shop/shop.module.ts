a88b8ce02ca03bf06451ffacdfcf24de6c3a9125
Dossumov <UDossumov@poland.luxoft.com>
2020-05-11 18:50:07 +0200

    9.102 refactored filtering

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,17 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { PagerComponent } from '../shared/components/pager/pager.component';
+import { PagingHeaderComponent } from '../shared/components/paging-header/paging-header.component';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
-  imports: [
-    CommonModule,
-    SharedModule
-  ],
-  exports: [
-    ShopComponent
-  ]
+  declarations: [ShopComponent, ProductItemComponent, PagerComponent, PagingHeaderComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [ShopComponent],
 })
-export class ShopModule { }
+export class ShopModule {}
