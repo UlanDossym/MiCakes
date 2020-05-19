fde1cb525a8b3e2ad2d15066987311bde89548f2
UlanDossym <dossumov.ulan@gmail.com>
2020-05-19 17:45:03 +0200

    156. added order summary and the ability to increment/decrement qty of items in basket

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -39,6 +39,43 @@ export class BasketService {
   getCurrentBasketValue() {
     return this.basketSource.value;
   }
+  incrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex((i) => i.id === item.id);
+    basket.items[foundItemIndex].quantity++;
+    this.setBasket(basket);
+  }
+  decrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex((i) => i.id === item.id);
+    if (basket.items[foundItemIndex].quantity > 1) {
+      basket.items[foundItemIndex].quantity--;
+      this.setBasket(basket);
+    } else {
+      this.removeItemFromBasket(item);
+    }
+  }
+  removeItemFromBasket(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    if (basket.items.some(x => x.id === item.id)) {
+      basket.items = basket.items.filter(p => p.id !== item.id);
+      if (basket.items.length > 0) {
+        this.setBasket(basket);
+      } else {
+        this.deleteBasket(basket);
+      }
+    }
+  }
+  deleteBasket(basket: IBasket) {
+    return this.httpClient.delete(this.baseUrl + 'basket?id=' + basket.id)
+      .subscribe(() => {
+        this.basketSource.next(null);
+        this.basketTotalSource.next(null);
+        localStorage.removeItem('basket_id');
+      }, error => {
+        console.log(error);
+      });
+  }
   addItemToBasket(item: IProduct, quantity = 1) {
     const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(
       item,
@@ -88,6 +125,6 @@ export class BasketService {
     const shipping = 0;
     const subTotal = basket.items.reduce((a, b) => b.price * b.quantity + a, 0);
     const total = subTotal + shipping;
-    this.basketTotalSource.next({shipping, total, subTotal});
+    this.basketTotalSource.next({ shipping, total, subTotal });
   }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [OrderTotalsComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot()
   ],
-  exports: [PaginationModule, CarouselModule],
+  exports: [PaginationModule, CarouselModule, OrderTotalsComponent],
 })
 export class SharedModule {}
