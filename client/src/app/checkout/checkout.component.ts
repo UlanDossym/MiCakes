7ba52a8b6e024068583e90484da655cfe71ac0aa
UlanDossym <dossumov.ulan@gmail.com>
2020-06-03 11:55:47 +0200

    253. refactored order summary window

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../account/account.service';
+import { IBasketTotals } from '../shared/models/basket';
+import { Observable } from 'rxjs';
+import { BasketService } from '../basket/basket.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
   checkoutForm: FormGroup;
-  constructor(private fb: FormBuilder, private accountService: AccountService) { }
+  basketTotals$: Observable<IBasketTotals>;
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private basketService: BasketService
+  ) {}
 
   ngOnInit() {
+    this.basketTotals$ = this.basketService.basketTotal$;
     this.createCheckoutForm();
     this.getAddressFormValues();
   }
@@ -23,24 +32,26 @@ export class CheckoutComponent implements OnInit {
         street: [null, Validators.required],
         city: [null, Validators.required],
         state: [null, Validators.required],
-        zipcode: [null, Validators.required]
+        zipcode: [null, Validators.required],
       }),
       deliveryForm: this.fb.group({
-        deliveryMethod: [null, Validators.required]
+        deliveryMethod: [null, Validators.required],
       }),
       paymentForm: this.fb.group({
-        nameOnCard: [null, Validators.required]
-      })
+        nameOnCard: [null, Validators.required],
+      }),
     });
   }
   getAddressFormValues() {
-    this.accountService.getUserAddress().subscribe(address => {
-      if (address) {
-        this.checkoutForm.get('addressForm').patchValue(address);
+    this.accountService.getUserAddress().subscribe(
+      (address) => {
+        if (address) {
+          this.checkoutForm.get('addressForm').patchValue(address);
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    }, error => {
-      console.log(error);
-    });
+    );
   }
-
 }
